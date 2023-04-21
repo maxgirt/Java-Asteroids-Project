5457a0683b0dfd29fce78aa15f3bac2c2bb1447f
ZRQ-rikkie <74203373+ZRQ-rikkie@users.noreply.github.com>
2023-04-21 12:37:27 +0100

    Add files via upload

---
+++ final2/Main.java
@@ -0,0 +1,21 @@
+package Asteroids2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+//this is the start of our java application
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        // create an instance of the menu screen
+        Menu menu = new Menu();
+        menu.start(primaryStage);
+    }
+
+    public static void main(String[] args) {
+        // launch the JavaFX application
+        launch(args);
+    }
+}
